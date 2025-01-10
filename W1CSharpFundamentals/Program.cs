2b1b8011666ba4c50ddee247caea926c15ce3974
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-01-10 16:29:04 -0500

    literal values

--- W1CSharpFundamentals/Program.cs
+++ W1CSharpFundamentals/Program.cs
@@ -2,9 +2,19 @@
 {
     internal class Program
     {
+        static int x;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int @float;
+            int y;
+
+            int i = 454;
+            double d = 45.45;
+            float f = 34.4F;
+            decimal m = 5.5M;
+
+            long l = 4543564L;
         }
     }
 }
